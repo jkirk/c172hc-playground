1c22e701a9637282b1da36b2b21d4be55b0b4042
Darshaka Pathirana <dpat@syn-net.org>
2023-04-30 15:04:48 +0200

    fsconnect_checksum.py: Fix flake8 and black errors
      ❯ flake8 --max-line-length 88 fsconnect_checksum.py
      fsconnect_checksum.py:14:1: E302 expected 2 blank lines, found 1
      fsconnect_checksum.py:21:10: E211 whitespace before '('
      fsconnect_checksum.py:22:10: E211 whitespace before '('
      fsconnect_checksum.py:24:1: E302 expected 2 blank lines, found 1
      fsconnect_checksum.py:30:1: E302 expected 2 blank lines, found 1
      fsconnect_checksum.py:31:10: E211 whitespace before '('
      fsconnect_checksum.py:35:10: E211 whitespace before '('
      fsconnect_checksum.py:37:1: E302 expected 2 blank lines, found 1
      fsconnect_checksum.py:38:10: E211 whitespace before '('
      fsconnect_checksum.py:44:1: E305 expected 2 blank lines after class or function definition, found 1

--- fsconnect_checksum.py
+++ fsconnect_checksum.py
@@ -5,37 +5,42 @@
 
 import argparse
 
-parser = argparse.ArgumentParser(description='This is an awesome python script!')
+parser = argparse.ArgumentParser(description="This is an awesome python script!")
 # parser.add_argument('argument', help='This is your argument')
 args = parser.parse_args()
 
-message_airspeed_demo = bytearray.fromhex('02 20 00 00 1D 80 42 03')
+message_airspeed_demo = bytearray.fromhex("02 20 00 00 1D 80 42 03")
+
 
 def create_message(gauge_type, gauge_value):
     message = bytearray(8)
     message[0] = 0x02
     message[1] = gauge_type
-    message[2:6] = gauge_value.to_bytes(4, 'big')
+    message[2:6] = gauge_value.to_bytes(4, "big")
     message[6] = checksum_xor(message[1:6])
     message[7] = 0x03
-    print (message.hex())
-    print (message_airspeed_demo.hex())
+    print(message.hex())
+    print(message_airspeed_demo.hex())
+
 
 def checksum_xor(_message):
-    _checksum_xor = 0xff
-    for char in (_message.removeprefix(b'0x02')):
+    _checksum_xor = 0xFF
+    for char in _message.removeprefix(b"0x02"):
         _checksum_xor = _checksum_xor ^ char
     return _checksum_xor
 
+
 def checksum_xor_stx(message):
-    print (message_airspeed_demo)
+    print(message_airspeed_demo)
 
 
 def checksum_8bit(message):
-    print (message_airspeed_demo)
+    print(message_airspeed_demo)
+
 
 def checksum_8bit_stx(message):
-    print (message_airspeed_demo)
+    print(message_airspeed_demo)
+
 
 # checksum_xor(message_airspeed_demo)
 # checksum_xor_stx(message_airspeed_demo)
