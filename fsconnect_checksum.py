6189aa0c8f68d58f3ae33738984a05f033de526d
Darshaka Pathirana <dpat@syn-net.org>
2023-05-01 12:09:42 +0200

    fsconntect_checksum.py: implement different checksum calculation
    create_message now supports different "checksum types", where the
    checksum is calculated differently:
    
    - 0 -> checksum: 0xFF XOR gauge_type XOR gauge_value
    - 1 -> checksum: 0xFF XOR 0x02 XOR gauge_type XOR gauge_value
    - 2 -> checksum: 0xFF - (0xFF + gauge_type + gauge_value) + 1
    - 3 -> checksum: 0xFF - (0xFF + 0x02 + gauge_type + gauge_value) + 1

--- fsconnect_checksum.py
+++ fsconnect_checksum.py
@@ -12,38 +12,70 @@ args = parser.parse_args()
 message_airspeed_demo = bytearray.fromhex("02 20 00 00 1D 80 42 03")
 
 
-def create_message(gauge_type, gauge_value):
+def create_message(gauge_type, gauge_value, checksum_type=0):
     message = bytearray(8)
     message[0] = 0x02
     message[1] = gauge_type
     message[2:6] = gauge_value.to_bytes(4, "big")
-    message[6] = checksum_xor(message[1:6])
+
+    if checksum_type == 0:
+        message[6] = checksum_xor(message[1:6])
+    elif checksum_type == 1:
+        message[6] = checksum_xor(message[0:6])
+    elif checksum_type == 2:
+        message[6] = checksum_8bit(message[1:6])
+    elif checksum_type == 3:
+        message[6] = checksum_8bit(message[0:6])
+
     message[7] = 0x03
-    print(message.hex())
-    print(message_airspeed_demo.hex())
+    return message.hex()
 
 
 def checksum_xor(_message):
     _checksum_xor = 0xFF
-    for char in _message.removeprefix(b"0x02"):
+    for char in _message:
         _checksum_xor = _checksum_xor ^ char
     return _checksum_xor
 
 
-def checksum_xor_stx(message):
-    print(message_airspeed_demo)
+def checksum_8bit(_message):
+    _sum = 0xFF
+    for char in _message:
+        _sum += char
+    _sum = _sum & 0xFF
+    _sum = checksum_xor(_sum.to_bytes(1, "big")) + 1
+    return _sum
 
 
-def checksum_8bit(message):
-    print(message_airspeed_demo)
-
-
-def checksum_8bit_stx(message):
-    print(message_airspeed_demo)
+def print_all_checksums(value):
+    print(
+        "Value: "
+        + str(value)
+        + " / XOR-Checksum w/o  STX: "
+        + create_message(0x20, value, 0)
+    )
+    # print("Value: "
+    #    + str(value) + " / XOR-Checksum w.  STX: " + create_message(0x20, value, 1))
+    print(
+        "Value: "
+        + str(value)
+        + " / 8bit-Checksum w/o STX: "
+        + create_message(0x20, value, 2)
+    )
+    print(
+        "Value: "
+        + str(value)
+        + " / 8bit-Checksum w.  STX: "
+        + create_message(0x20, value, 3)
+    )
 
 
 # checksum_xor(message_airspeed_demo)
 # checksum_xor_stx(message_airspeed_demo)
 # checksum_8bit(message_airspeed_demo)
 # checksum_8bit_stx(message_airspeed_demo)
-create_message(0x20, 7552)
+print("Airspeed Demo:        " + message_airspeed_demo.hex())
+
+print_all_checksums(0)
+print_all_checksums(102)
+print_all_checksums(7552)
